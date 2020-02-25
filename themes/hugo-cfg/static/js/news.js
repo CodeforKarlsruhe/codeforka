ee4a71f0f20f687e1bed3f9947663bd4d063adcd
Andreas Kugel <tab@akugel.de>
2020-02-25 10:13:25 +0100

    newsletter on website from json

---
+++ themes/hugo-cfg/static/js/news.js
@@ -0,0 +1,46 @@
+// try to read schedule from json
+
+$(document).ready(function(e) {
+  // load only on page with newsletter selector
+  if ($("#newsletter").length) {
+      //$.getJSON("/news/news.json", function(result){
+      $.ajaxSetup({ cache: false });
+      //var langUrl = "https://raw.githubusercontent.com/CodeforKarlsruhe/labNews/master/news.json";
+      var langUrl = "/news/news.json";
+          $.getJSON(langUrl, function(result){
+        if (0 == result.length)
+            console.log("No newsletter data")
+        else {
+            console.log("Length " + result.length)
+            if (lang && lang == "en") {
+                  var news = result.en
+            } else {
+                  var news = result.de
+            }
+
+            var sc = "<div class=\"newsletter\">";
+            sc += "<h1>" + news.title + "</h1>"
+            sc += news.date + "<br>"
+
+            $.each(news.items, function(i, field){
+                console.log("News: ",i)
+                sc += "<div class=\"" + ((i%2) ? "news-odd" : "news-even") + "\">"
+                sc += "<h2>" + field.headline + "</h2>"
+                sc += "<img class=\"news-img\" src=\"" + field.imglink + "\" + title=\"" + field.imgtitle + "\">"
+                sc += "<p>" + field.text + "</p>";
+                sc += "</div>"
+            });
+            sc += "</div>";
+            // append
+            //$("#newsletter").append(sc);
+            $("#newsletter").html(sc);
+        }
+      });
+  } else
+    console.log("No newsletter id")
+});
+
+// language toggle
+function langToggle() {
+    console.log("toggle")
+}
